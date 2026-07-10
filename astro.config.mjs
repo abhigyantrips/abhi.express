4593bd20ff1ab5646dab094a582d6ca1e1089290
Abhigyan Trips <contact@abhigyantrips.dev>
2026-07-11 00:40:41 +0530

    feat(deploy): add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,26 @@ import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
+	site: import.meta.env.DEV ? "http://localhost:4321" : "https://abhi.express",
+	output: "static",
+
+	session: {
+		driver: {
+			entrypoint: "unstorage/drivers/null",
+		},
+	},
+
+	adapter: cloudflare({
+		imageService: "cloudflare",
+	}),
+
+	integrations: [sitemap()],
+
 	vite: {
 		plugins: [tailwindcss()],
 	},
-
-	integrations: [sitemap()],
 });
